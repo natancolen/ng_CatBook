570d823bb1e07668fdf0857fc458c930f93fb17b
natancolen <natancolen@gmail.com>
2022-08-20 16:07:18 -0300

    login

--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -1,12 +1,19 @@
 import { HomeComponent } from './home.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
   {
     path: '',
-    component:HomeComponent,
-  }
+    component: HomeComponent,
+    children: [
+      {
+        path: '',
+        component: LoginComponent,
+      },
+    ],
+  },
 ];
 
 @NgModule({
